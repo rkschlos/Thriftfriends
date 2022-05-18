a81affb0c14e93bbc290aa6c1a2a2da83204085a
Rachael Schlosberg <rachael.schlosberg@gmail.com>
2022-05-18 18:43:14 -0500

    downloaded react-router-dom

--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -11,6 +11,24 @@ root.render(
   </React.StrictMode>
 );
 
+async function loadInventory() {
+  const brandResponse = await fetch('http://localhost:8100/api/brands/');
+  let brandData;
+
+  if (brandResponse.ok) {
+    brandData = await brandResponse.json();
+  } else {
+    console.error(brandResponse);
+  }
+
+  root.render(
+    <React.StrictMode>
+      <App
+        brands = {brandData.brands}
+      />
+    </React.StrictMode>
+  );
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
